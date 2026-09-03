5e2d8156283c35e5b8a340f391a67373e219a248
Breno Aleck <brenoaleckgomesribeiro@gmail.com>
2026-09-02 21:32:38 -0300

    Aula 2

---
+++ ex2.py
@@ -0,0 +1,7 @@
+username_cadastrado = "admin"
+senha_cadastrada = "1234"
+
+if input("Digite seu nome de usuário") == username_cadastrado and input("Digite sua senha") == senha_cadastrada:
+    print("Acesso Concedido")
+else:
+    print("Credenciais invalidas")
