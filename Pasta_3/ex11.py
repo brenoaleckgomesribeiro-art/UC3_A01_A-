6607f7d89d0463a14f67fa91a43ea0a439e1dd8d
Breno aleck <brenoaleckgomesribeiro@gmail.com>
2026-09-04 21:07:56 -0300

    Atualização da Aula 03

--- Pasta_3/ex11.py
+++ Pasta_3/ex11.py
@@ -1,18 +1,22 @@
+#a classe Filme representa um filme com título, duração e status de assistido.
 class Filme:
-
+    # def = Construtor da classe Filme que inicializa o título, duração e status de assistido.
     def __init__(self, titulo, duracao):
         self.titulo = titulo
         self.duracao = duracao
         self.assistido = False 
 
+    # def marcar_como_assistido = Método que marca o filme como assistido, alterando o status para True.
     def marcar_como_assistido(self):
         self.assistido = True
 
-
+# Exemplo de uso da classe Filme
 Gente_Grande = Filme("Gente Grande", 120)
 Todo_Mundo_Em_Panico = Filme("Todo Mundo em Pânico", 90)
 
+# Marcar o filme "Gente Grande" como assistido  
 Gente_Grande.marcar_como_assistido()
 
+# Verificar o status de assistido dos filmes
 print(Gente_Grande.assistido)  
 print(Todo_Mundo_Em_Panico.assistido)  
